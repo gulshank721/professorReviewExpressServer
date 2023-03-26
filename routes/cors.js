d045f9f3bf87fb57e4b4f99623bac3a1c74d71d0
Gulshan Kumar <gulshank721@gmail.com>
2023-03-26 10:48:56 +0530

    reviewsRouter updated

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3000','http://10.41.182.166:5501','http://localhost:3001','http://localhost:5501','https://gulshankrestaurent.netlify.app'];
+const whitelist = ['http://localhost:3000','http://10.41.182.166:5501','http://localhost:3001','http://localhost:5501','http://10.41.179.91:5501'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
--- routes/reviewsRouter.js
+++ routes/reviewsRouter.js
@@ -17,7 +17,7 @@ reviewsRouter.use(bodyParser.json());
 reviewsRouter.route('/')
 .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
 .get(cors.cors,(req,res,next)=>{
-
+    console.log("req.query",req.query);
     Reviews.find(req.query)// this is mongoose function
     // .populate('comments.author') // including the author field in commments.
     .then((reviews) => {
@@ -38,5 +38,53 @@ reviewsRouter.route('/')
     .catch((err) => next(err));
 })
 
+reviewsRouter.route("/search")
+.options( cors.corsWithOptions, (req,res)=>{res.statusCode=(200);})
+.get(cors.cors,(req, res,next) => {
+    console.log('Request body',req.query);
+    const query = req.query.q;
+    console.log('Request query',query);
+    //building search query for our database 
+    const searchQuery = {
+      $or: [
+        { professorName: { $regex: query, $options: 'i' } }, // Search in title field
+        { instituteName: { $regex: query, $options: 'i' } }, // Search in content field
+        { instituteState: {$regex: query, $options: 'i' } } ,// search in state field
+        { instituteCity: {$regex: query, $options: 'i' } }
+      ]
+    };
+  
+    // Build the search query object
+  //  const searchQuery = {
+  //   $text: {
+  //     $search: query
+  //   }
+  // };
+    console.log('SearchQuery',searchQuery);
+    Reviews.find(searchQuery)// this is mongoose function
+    .then((reviews) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: true,
+          results: reviews});
+    }, (err) => next(err))
+    .catch((err) => next(err));
+    ////
+    // try {
+    //   const results = await Reviews.find(searchQuery);
+    //   console.log(results);
+    //   res.json({
+    //     success: true,
+    //     results: results
+    //   });
+    // } catch (error) {
+    //   console.error("catched Error",error);
+    //   res.status(500).json({
+    //     success: false,
+    //     message: 'Server error '+`${error}`,
+    //     query: query
+    //   });
+    // }
+  });
 
 module.exports = reviewsRouter;
