56fefb94d7e7525207fb89a2f8510ac9be9a7103
Gulshan Kumar <gulshank721@gmail.com>
2023-03-29 13:32:55 +0530

    Cors update 2

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3000','http://10.41.182.166:5501','http://localhost:3001','http://localhost:5501','http://10.41.179.91:5501'];
+const whitelist = ['http://localhost:3000','http://10.41.182.166:5501','http://localhost:3001','http://localhost:5501','http://10.41.179.91:5501', 'https://gulshank721.github.io/professorReviews'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
