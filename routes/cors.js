5b5683d3ed1e5f619604ea1b994efbba524dc96d
Gulshan Kumar <gulshank721@gmail.com>
2023-10-23 20:58:41 +0530

    Azure url added into  cors

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3000','http://10.41.182.166:5501','http://localhost:3001','http://localhost:5501','http://10.41.179.91:5501', 'https://gulshank721.github.io/professorReviews','https://gulshank721.github.io,https://researchersreviews.azurewebsites.net/reviews'];
+const whitelist = ['http://localhost:3000','http://10.41.182.166:5501','http://localhost:3001','http://localhost:5501','http://10.41.179.91:5501', 'https://gulshank721.github.io/professorReviews','https://gulshank721.github.io','https://researchersreviews.azurewebsites.net/reviews'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
